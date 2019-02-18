b7c52c3d76c9ce4146f6e1f789a38dc58efc7c7d
Marta Ginosyan <marta.ginosian@gmail.com>
2019-02-18 13:20:47 +0400

    Adds info endpoint.

--- notes-facade/src/main/java/com/margin/disqo/rest/InfoEndpoint.java
+++ notes-facade/src/main/java/com/margin/disqo/rest/InfoEndpoint.java
@@ -1,4 +1,12 @@
 package com.margin.disqo.rest;
 
-public class InfoEndpoint {
+import com.margin.disqo.dto.InfoDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(path = "/info")
+public interface InfoEndpoint {
+    @GetMapping(path = "")
+    ResponseEntity<InfoDTO> info();
 }
--- notes-facade/src/main/java/com/margin/disqo/rest/impl/InfoEndpointImpl.java
+++ notes-facade/src/main/java/com/margin/disqo/rest/impl/InfoEndpointImpl.java
@@ -1,4 +1,19 @@
 package com.margin.disqo.rest.impl;
 
-public class InfoEndpointImpl {
+import com.margin.disqo.dto.InfoDTO;
+import com.margin.disqo.rest.InfoEndpoint;
+import com.margin.disqo.service.InfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class InfoEndpointImpl implements InfoEndpoint {
+
+    @Autowired
+    private InfoService infoService;
+
+    @Override
+    public ResponseEntity<InfoDTO> info() {
+        return new ResponseEntity<>(infoService.get(), HttpStatus.OK);
+    }
 }
